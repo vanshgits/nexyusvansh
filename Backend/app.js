36acb67d8bedbe7d2c420a647962d36ebd0fccf8
Vedbhimani <133159910+Vedbhimani@users.noreply.github.com>
2025-11-13 19:02:47 -0500

    IC

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json()); // to read JSON body
+
+const customerRoutes = require('./routes/customers');
+app.use('/customers', customerRoutes);
+
 app.get('/', (req, res) => {
     res.send("Welcome to Backend");
 });
